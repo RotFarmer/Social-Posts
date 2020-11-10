f1f67e8f64147949bc36a140ba970ce87f257b0a
rotfarmer <datyosefguy@gmail.com>
2020-11-10 16:57:18 -0500

    styled and added popup function

--- src/app/social-posts/social-posts.component.ts
+++ src/app/social-posts/social-posts.component.ts
@@ -14,13 +14,21 @@ export class SocialPostsComponent implements OnInit {
         "welcome to social posts, a place to post socially. why not share your thought's and opinions?",
     },
   ];
+  popup:number=null;
   constructor() {}
 
   ngOnInit(): void {}
   onSubmit = (post: Post): void => {
     this.posts.push(post);
+    this.popup=null
   };
   onDelete = (index: number): void => {
     this.posts.splice(index, 1);
   };
+  popupValue = ()=>{
+    this.popup=0
+  }
+  darkClick = ()=>{
+    this.popup=null
+  }
 }
